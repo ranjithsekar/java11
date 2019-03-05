8459978a86c7a6d36997bc3ee67388527fa21391
Ranjith Sekar <ranjith.sekar@clarivate.com>
2019-03-05 19:39:55 +0530

    up

--- java11-modules-order/src/jbr/java11/order/phone/OrderPhone.java
+++ java11-modules-order/src/jbr/java11/order/phone/OrderPhone.java
@@ -1,11 +1,11 @@
 package jbr.java11.order.phone;
 
-//import jbr.java11.payment.Payment;
+import jbr.java11.payment.Payment;
 
 public class OrderPhone {
 
-	public void orderPhone() {
-		System.out.println("Order Phone");
-		//new Payment().pay();
-	}
+  public void orderPhone() {
+    System.out.println("Order Phone");
+    new Payment().pay();
+  }
 }
--- java11-modules-order/src/module-info.java
+++ java11-modules-order/src/module-info.java
@@ -1,4 +1,6 @@
 module shop.order {
-	exports jbr.java11.order.phone;
-//	exports jbr.java11.order.jewel;
+  exports jbr.java11.order.phone;
+  // exports jbr.java11.order.jewel;
+
+  requires shop.payment;
 }
